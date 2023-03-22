bf72512649798bd1c8dc138f2fc7e1e4a557da23
xiaoyun.zhang <cloudone.zhang@garena.com>
2023-03-22 14:18:50 +0800

    add content menu bar

--- main.py
+++ main.py
@@ -3,22 +3,37 @@ from tkinter import filedialog
 
 # Create a new window
 root = tk.Tk()
-root.title("Text File Reader")
+root.title("Ume TXT Reader")
+
 
 # Define a function to open a file
 def open_file():
-    file_path = filedialog.askopenfilename()
-    with open(file_path, "r") as file:
+    root.filename = filedialog.askopenfilename(initialdir="/", title="Select A txt File",
+                                               filetypes=[("txt files", "*.txt")])
+    if not root.filename:
+        print("No file selected!")
+        return
+    with open(root.filename, "r") as file:
         text.delete("1.0", tk.END)
         text.insert("1.0", file.read())
 
+
+def warp_content():
+    print("Content")
+
+
 # Create a menu bar
 menubar = tk.Menu(root)
-filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=open_file)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
+file_menu = tk.Menu(menubar, tearoff=0)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=file_menu)
+
+content_menu = tk.Menu(menubar, tearoff=0)
+content_menu.add_command(label="Get Content", command=warp_content)
+menubar.add_cascade(label="Content", menu=content_menu)
+
 root.config(menu=menubar)
 
 # Create a text widget to display the contents of the file
